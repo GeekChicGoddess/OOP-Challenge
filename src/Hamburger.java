a088eb5341af395b16d8046c172b10b52cc86e32
melodytempleton <mtempletoncantu@gmail.com>
2017-05-31 11:53:40 -0500

    Hamburger with methods complete

--- src/Hamburger.java
+++ src/Hamburger.java
@@ -3,14 +3,53 @@
  */
 public class Hamburger {
 
-    private String rolltype = "";
+    private String rollType = "";
     private String meat = "";
-    private double price = 0d;
+    private Double price = 0d;
 
-    public Hamburger (String rolltype, String meat, double price) {
-        this.rolltype = rolltype;
+
+    public Hamburger (String rollType, String meat) {
+        this.rollType = rollType;
         this.meat = meat;
-        this.price = price;
+        this.price = 3.00d;
+
+        System.out.println("You have ordered a " + this.meat + " burger on a "  + this.rollType + " at base price of " + this.price);
+    }
+
+      public void addCheese () {
+
+        this.price = this.price + 1.00d;
+        System.out.println("Added cheese to burger for additional $1.00");
+          giveBurgerTotal ();
+    }
+
+      public void addBacon () {
+
+        this.price = this.price + 1.00d;
+        System.out.println("Added bacon to burger for additional $1.00");
+          giveBurgerTotal ();
+
+    }
+
+      public void addLettuce () {
+
+        this.price = this.price + .50d;
+        System.out.println("Added lettuce to burger for additional $.50");
+          giveBurgerTotal ();
+
+    }
+
+      public void addTomato() {
+
+        this.price = this.price + .50d;
+        System.out.println("Added tomato to burger for additional $1.00");
+          giveBurgerTotal ();
+
+    }
+
+    public void giveBurgerTotal (){
+
+        System.out.println("Your burger price is $" + this.price);
     }
 
 
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by melodytempleton on 5/31/17.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Hamburger hamburger = new Hamburger("rare", "keiser");
+        hamburger.addCheese();
+
+
+
+    }
+}
