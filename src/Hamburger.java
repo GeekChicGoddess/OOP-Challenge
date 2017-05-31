2f53f45a86459c6e3546d708c94450cc189bc962
melodytempleton <mtempletoncantu@gmail.com>
2017-05-31 11:12:21 -0500

    Initial commit Setup base Hamburger class

---
+++ src/Hamburger.java
@@ -0,0 +1,17 @@
+/**
+ * Created by melodytempleton on 5/31/17.
+ */
+public class Hamburger {
+
+    private String rolltype = "";
+    private String meat = "";
+    private double price = 0d;
+
+    public Hamburger (String rolltype, String meat, double price) {
+        this.rolltype = rolltype;
+        this.meat = meat;
+        this.price = price;
+    }
+
+
+}
